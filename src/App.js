ef4f9428ff229dd52bdba07032df28d14c2f9b92
mtking78 <mtking78@gmail.com>
2019-07-09 08:43:28 -0700

    Recipes component and recipe api.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./Components/Form";
+import Recipes from "./Components/Recipes";
+
+const API_KEY = "bb89b65280b78567a81f76e66875eac9";
 
 class App extends Component {
 
-  getRecipe = (e) => {
-    e.preventDefault();
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
-    console.log(recipeName);
+    e.preventDefault();
+
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   }
 
   render() {
@@ -16,7 +28,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
+        <Form getRecipe={ this.getRecipe }/>
+        <Recipes recipes={ this.state.recipes }/>
       </div>
     );
   }
