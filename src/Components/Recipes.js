0e99601e5cd2fcb6c42ae98c917a1793e7f9eb70
mtking78 <mtking78@gmail.com>
2019-07-09 09:43:50 -0700

    css applied to form and recipe components.

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
 const Recipes = props => (
-  <div>
-    { props.recipes.map((recipe) => {
-      return (
-        <div key={ recipe.recipe_id }>
-          <img src={ recipe.image_url } alt={ recipe.title }/>
-          <p>{ recipe.title }</p>
-        </div>
-      );
-    })
-    }
+  <div className="container">
+    <div className="row">
+      { props.recipes.map((recipe) => {
+        return (
+          <div
+            className="col-md-4"
+            key={ recipe.recipe_id }
+            style={{ marginBottom:"2rem" }}
+          >
+            <div className="recipe_box">
+              <img
+                className="recipe_box-img"
+                src={ recipe.image_url }
+                alt={ recipe.title }
+              />
+              <div className="recipe_text">
+                <h5 className="recipe_title">
+                  {/* Keep titles reduced to 18 characters max */}
+                  { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 18)}...` }
+                </h5>
+                <p className="recipe_subtitle">
+                  Publisher: <span>{ recipe.publisher }</span>
+                </p>
+              </div>
+              <button className="recipe_button">View Recipe</button>
+            </div>
+          </div>
+        );
+      })
+      }
+    </div>
   </div>
 );
 
